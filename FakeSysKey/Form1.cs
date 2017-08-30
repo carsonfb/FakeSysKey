b2c36376463b600b5829279470a7c814d25975ba
Carson F. Ball <carson.ball@gmail.com>
2017-08-30 13:04:50 -0500

    Added functionality to the OK button on the main form so that it closes the window.

--- FakeSysKey/Form1.cs
+++ FakeSysKey/Form1.cs
@@ -19,7 +19,7 @@ namespace FakeSysKey
 
         private void button1_Click(object sender, EventArgs e)
         {
- 
+            Close();
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
